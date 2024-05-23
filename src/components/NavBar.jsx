83fd9d7399179cb4937a8249ab803be710436418
TAMAN <taman.hansalia@gmail.com>
2024-05-23 21:43:16 +0530

    updates

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { BsChatSquareDots } from 'react-icons/bs'
+import {}
+
+
+
+
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
