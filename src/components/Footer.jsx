b3e5cfda05669eea644ab8323482511b1357f9c4
Taman Hansalia <Taman Hansalia>
2024-06-13 13:01:29 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,11 @@ const Footer = () => {
   return (
     <div className="max-w-[1140px] w-full py-8 m-auto border-t-4">
       <div className="flex items-center m-auto justify-center">
-        <BsChatSquareDots size={30} className="text-[var(--primary-dark)] mr-2" />
-        <h1 className="text-xl font-bold text-gray-700">WEEKAWAY</h1>
+        <BsChatSquareDots
+          size={30}
+          className="text-[var(--primary-dark)] mr-2"
+        />
+        <h1 className="text-xl font-bold text-gray-700">WaveAway</h1>
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
       <div className="max-w-[1140px] m-auto">
         <div className=" absolute top-[40%] w-full md:-[50%] max-w-[600px] h-full flex flex-col text-white p-4">
           <h1 className="font-bold text-4xl">Find your Trip</h1>
-          <h2 className="text-4xl py-4 italic">with weekaway</h2>
+          <h2 className="text-4xl py-4 italic">with WaveAway</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. At, maxime
             culpa totam esse cumque hic ea veritatis quibusdam sit velit ullam,
